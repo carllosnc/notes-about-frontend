83e20455aa656cc8ed0c025ab5804cb9abc4b349
Carlos Costa <carllos.nc@gmail.com>
2023-03-13 11:06:52 -0300

    register global components

--- vue-ts/src/App.vue
+++ vue-ts/src/App.vue
@@ -1,24 +1,4 @@
 <script setup lang="ts">
-import CounterOptions from './topics/01-options-style.vue'
-import CounterComposition from './topics/02-composition-style.vue'
-import TemplateSyntax from './topics/05-template-syntax.vue'
-import Reactivity from './topics/03-reactivity.vue'
-import ComputedProperties from './topics/04-computed-properties.vue'
-import StyleBinding from './topics/06-style-binding.vue'
-import ConditionalRendering from './topics/07-conditional-rendering.vue'
-import ListRendering from './topics/08-list-rendering.vue'
-import EventHandling from './topics/09-event-handling.vue'
-import FormInputBinding from './topics/10-form-input-binding.vue'
-import LifecycleHooks from './topics/11-lifecycle-hooks.vue'
-import Watchers from './topics/12-watchers.vue'
-import TemplateRef from './topics/13-template-ref.vue'
-
-// components
-
-import PassingProps from './topics/components/01-passing-props.vue'
-import ListeringToEvents from './topics/components/02-listering-to-events.vue'
-import Slots from './topics/components/03-slots.vue'
-
 const posts = [
   {
     title: 'My journey with Vue',
@@ -66,5 +46,7 @@ function fn(arg: string) {
     :content="post.content"
   /> -->
 
-  <Slots> <h1 class="text-red-500 font-bold">Hello world</h1> </Slots>
+  <Slots>
+    <h1 class="text-red-500 font-bold">Hello world</h1>
+  </Slots>
 </template>
